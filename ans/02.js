c6bed3da4759a594d473615f4c3379f1aab0c936
dennishoooo <105833202+dennishoooo@users.noreply.github.com>
2022-12-04 12:33:26 +0800

    add 02 ans

---
+++ ans/02.js
@@ -0,0 +1,26 @@
+let pattern0 = "+-+-+-+-+-+";
+
+let pattern1 = "-+-+-+-+-";
+let pattern2 = "-+-+-";
+
+// start ++
+// end --
+
+function printReverseTriangle(pattern) {
+  let start = 0;
+  let end = pattern.length;
+  let space = "";
+
+  while (end > start) {
+    console.log(space + pattern.slice(start, end));
+    start++;
+    end--;
+    space += " ";
+  }
+}
+printReverseTriangle(pattern0);
+
+printReverseTriangle(pattern1);
+printReverseTriangle(pattern2);
+
+// console.log(pattern.slice(2, pattern.length - 2));
